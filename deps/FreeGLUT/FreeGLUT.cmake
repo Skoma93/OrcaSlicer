4ec1b5ec2fa1efc437086eb09db634bfea7c1c6c
Skoma993 <skorkaoliver93@gmail.com>
2024-11-15 19:34:51 +0100

    freeGLUT added to deps

--- deps/FreeGLUT/FreeGLUT.cmake
+++ deps/FreeGLUT/FreeGLUT.cmake
@@ -1,6 +1,6 @@
 find_package(OpenGL QUIET REQUIRED)
 
-if (APPLE)
+if(APPLE)
     message(STATUS "Compiling TIFF for macos ${CMAKE_SYSTEM_VERSION}.")
     orcaslicer_add_cmake_project(TIFF
         URL https://gitlab.com/libtiff/libtiff/-/archive/v4.3.0/libtiff-v4.3.0.zip
@@ -13,21 +13,45 @@ if (APPLE)
             -Dzstd:BOOL=OFF
             -Dlibdeflate:BOOL=OFF
             -Dpixarlog:BOOL=OFF
+        -Dlzma:BOOL=OFF
+        -Dwebp:BOOL=OFF
+        -Djbig:BOOL=OFF
+        -Dzstd:BOOL=OFF
+        -Dlibdeflate:BOOL=OFF
+        -Dpixarlog:BOOL=OFF
     )
-else()
+elseif(MSVC)
     orcaslicer_add_cmake_project(TIFF
         URL https://gitlab.com/libtiff/libtiff/-/archive/v4.1.0/libtiff-v4.1.0.zip
         URL_HASH SHA256=c56edfacef0a60c0de3e6489194fcb2f24c03dbb550a8a7de5938642d045bd32
         DEPENDS ${ZLIB_PKG} ${PNG_PKG} ${FreeGLUT_PKG} dep_JPEG
+        CMAKE_ARGS
+        -Dlzma:BOOL=OFF
+        -Dwebp:BOOL=OFF
+        -Djbig:BOOL=OFF
+        -Dzstd:BOOL=OFF
+        -Dpixarlog:BOOL=OFF
+    ) 
+else()
+    orcaslicer_add_cmake_project(TIFF
+        URL https://gitlab.com/libtiff/libtiff/-/archive/v4.1.0/libtiff-v4.1.0.zip
+        URL_HASH SHA256=c56edfacef0a60c0de3e6489194fcb2f24c03dbb550a8a7de5938642d045bd32
+        DEPENDS ${ZLIB_PKG} ${PNG_PKG} dep_JPEG
         CMAKE_ARGS
             -Dlzma:BOOL=OFF
             -Dwebp:BOOL=OFF
             -Djbig:BOOL=OFF
             -Dzstd:BOOL=OFF
             -Dpixarlog:BOOL=OFF
+        -Dlzma:BOOL=OFF
+        -Dwebp:BOOL=OFF
+        -Djbig:BOOL=OFF
+        -Dzstd:BOOL=OFF
+        -Dpixarlog:BOOL=OFF
+        -DGLUT_INCLUDE_DIR=sys
+        -DGLUT_glut_LIBRARY=sys/freeglut.lib
+        -DCMAKE_C_FLAGS="-I${DESTDIR}/include"
+        -DCMAKE_C_FLAGS="/Dstrncasecmp=_strnicmp"
     )
 
 endif()
-
-
-
