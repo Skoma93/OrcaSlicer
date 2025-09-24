fe143e47507368c88c5b889388c83103d22f05d6
SkomaCraft <oliver.skorka@craftbot.com>
2025-09-24 12:27:38 +0200

    custom version added

--- src/slic3r/GUI/GUI_App.cpp
+++ src/slic3r/GUI/GUI_App.cpp
@@ -4650,7 +4650,7 @@ std::string GUI_App::format_display_version()
 {
     if (!version_display.empty()) return version_display;
 
-    version_display = SoftFever_VERSION;
+    version_display = Skoma_VERSION;
     return version_display;
 }
 
