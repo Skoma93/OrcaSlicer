79994ac2dc9d9cd72fd09739e6b9b087c951a847
SkomaCraft <oliver.skorka@craftbot.com>
2026-08-04 08:50:50 +0200

    build fix

--- deps/PNG/PNG.cmake
+++ deps/PNG/PNG.cmake
@@ -12,7 +12,7 @@ if(APPLE AND IS_CROSS_COMPILE)
         GIT_TAG v1.6.35
         DEPENDS ${ZLIB_PKG}
         PATCH_COMMAND       ${GIT_EXECUTABLE} checkout -f -- . && git clean -df &&
-                            ${GIT_EXECUTABLE} apply --whitespace=fix ${CMAKE_CURRENT_LIST_DIR}/macos-arm64.patch ${CMAKE_CURRENT_LIST_DIR}/0002-clang19-macos.patch
+                            ${GIT_EXECUTABLE} apply --whitespace=fix --ignore-space-change --ignore-whitespace ${CMAKE_CURRENT_LIST_DIR}/macos-arm64.patch ${CMAKE_CURRENT_LIST_DIR}/0002-clang19-macos.patch
         CMAKE_ARGS
             -DPNG_SHARED=OFF
             -DPNG_STATIC=ON
